b150ecf40bd54e291d3b709dbc9bae5e3c0ec519
Mata <mata@pop-os.localdomain>
2023-10-06 22:23:01 +0300

    Add Background

--- flapy/init.go
+++ flapy/init.go
@@ -1,21 +1,29 @@
 package flapy
 
-import raylib "github.com/gen2brain/raylib-go/raylib"
+import rl "github.com/gen2brain/raylib-go/raylib"
 
 var (
 	screenWidth  int32 = 800
 	screenHeight int32 = 450
 )
 
-func Init() {
-	raylib.InitWindow(screenWidth, screenHeight, "flapy bird")
-	raylib.SetTargetFPS(60)
-	defer raylib.CloseWindow()
-	for !raylib.WindowShouldClose() {
-		raylib.BeginDrawing()
-		raylib.ClearBackground(raylib.White)
-		raylib.DrawText("Hello To Flapy bird.", 100, 200, 16*2, raylib.Red)
+func loadTexture(path string) rl.Texture2D {
+	texture2D := rl.LoadTexture(path)
+	return texture2D
+}
 
-		raylib.EndDrawing()
+func Init() {
+	rl.InitWindow(screenWidth, screenHeight, "flapy bird")
+	rl.SetTargetFPS(60)
+	defer rl.CloseWindow()
+	bgTexture := loadTexture("assets/Background.png")
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.White)
+
+		rl.DrawTexture(bgTexture, screenHeight/2-bgTexture.Width/2, screenHeight/2-bgTexture.Height/2, rl.White)
+
+		rl.EndDrawing()
 	}
 }
